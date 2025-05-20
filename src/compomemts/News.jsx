a4f5d5a6987f2ac0b79578991a2b64c66f7fe7a5
Nuzler <86511139+Nuzler@users.noreply.github.com>
2025-05-21 00:16:20 +0530

    Update News.jsx

--- src/compomemts/News.jsx
+++ src/compomemts/News.jsx
@@ -24,7 +24,7 @@ const buttonClick=()=>{
 console.log(limit)
 
 useEffect(()=>{
-  fetch(`https://backend-kn0qha.fly.dev/api/news/${limit?'home':'all'}`)
+  fetch(`https://rogcryptobackend-648491182682.europe-west1.run.app/api/news/${limit?'home':'all'}`)
   .then(response=>response.json())
   .then(data=>setNews(data))
   .catch(error => console.error("Error fetching foods:", error));
